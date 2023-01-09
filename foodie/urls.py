575dbf7dab53479e542e2c66e829c8b838192e33
ookuboc5399 <ankle0614@gmail.com>
2023-01-09 10:04:14 +0900

    0109

---
+++ foodie/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('search/', views.Search, name='search'),
+    path('shop_info/<str:restid>', views.ShopInfo, name='shop_info')
+]
